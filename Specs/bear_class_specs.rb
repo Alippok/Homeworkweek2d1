51426c6b058b342b050adce22d501c86450ba659
A_Lippok <alippok@hotmail.co.uk>
2016-02-22 17:15:02 +0000

    Passes test to get bears name

--- Specs/bear_class_specs.rb
+++ Specs/bear_class_specs.rb
@@ -1,16 +1,15 @@
 require("minitest/autorun")
-require_relative("./bear_class.rb")
+require_relative("../bear_class.rb")
 
 class TestBearClass < Minitest::Test
 
 def setup
-  bear1 = Bear.new("Yogi", "Grizzly", "Rooooar!", [])
+  @bear = Bear.new("Yogi", "Grizzly", "Rooooar!", [])
 
 end
 
 def test_bear_name
-  
-
+  assert_equal("Yogi", @bear.name)
 end
 
 
