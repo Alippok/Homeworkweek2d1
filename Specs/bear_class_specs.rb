63edc783583cf94ec5f58bff9dcb6cf3f5819f79
A_Lippok <alippok@hotmail.co.uk>
2016-02-22 17:24:44 +0000

    Adds test for code to add things to bear tummy-array

--- Specs/bear_class_specs.rb
+++ Specs/bear_class_specs.rb
@@ -12,6 +12,20 @@ def test_bear_name
   assert_equal("Yogi", @bear.name)
 end
 
+def test_bear_roar
+  assert_equal("Rooooar!", @bear.roar)
+end
+
+def test_bear_type
+  assert_equal("Grizzly", @bear.type)
+end
+
+def test_set_bear_tummy
+  @bear.tummy = "Fish1", "Fish2", "Fish3", "Fish4"
+  assert_equal(["Fish1", "Fish2", "Fish3", "Fish4"], @bear.tummy)
+end
+
+
 
 
 
